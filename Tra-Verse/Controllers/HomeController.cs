70c35451561ed31fb34b63589da15a85ac9660f9
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-11-30 10:53:19 -0500

    added first comment

--- Tra-Verse/Controllers/HomeController.cs
+++ Tra-Verse/Controllers/HomeController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//testing this commit
 namespace Tra_Verse.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            ViewBag.Message = "whats up losers";
             return View();
         }
 
