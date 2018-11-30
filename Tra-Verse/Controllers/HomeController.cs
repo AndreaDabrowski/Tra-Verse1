7049c95c7e0fa60ac6922c21a1baf2183a9cba79
Johnathan Leech <johnathanleech09@gmail.com>
2018-11-30 11:43:12 -0500

    got rid of unnecessary views and changed _Layout

--- Tra-Verse/Controllers/HomeController.cs
+++ Tra-Verse/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 //testing this commit
 namespace Tra_Verse.Controllers
 {
@@ -10,7 +6,7 @@ namespace Tra_Verse.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "whats up losers";
+            ViewBag.Title = "Always Moving Forward";
             return View();
         }
 
