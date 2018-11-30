51d7e966c6df85e847822ccb1b14f75e89820dcd
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-11-30 10:38:11 -0500

    testing first commit

---
+++ Tra-Verse/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Tra_Verse
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Tra-Verse/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tra_Verse.MvcApplication" Language="C#" %>
