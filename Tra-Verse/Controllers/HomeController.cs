0c28b50d5c08eb4ac6b15d945385969e3e7b32ca
Johnathan Leech <johnathanleech09@gmail.com>
2018-11-30 16:29:34 -0500

    added parsec info for planets

--- Tra-Verse/Controllers/HomeController.cs
+++ Tra-Verse/Controllers/HomeController.cs
@@ -47,30 +47,17 @@ namespace Tra_Verse.Controllers
 
         public ActionResult SkyScanner()
         {
-            //1. request and response, setting up to make the call out to the API
             string nasaAPIKey = System.Configuration.ConfigurationManager.AppSettings["NASA API Header"];
-            HttpWebRequest request = WebRequest.CreateHttp("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&format=json");
-            //+nasaAPIKey
+            HttpWebRequest request = WebRequest.CreateHttp("https://images-api.nasa.gov");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-            //2. Reading the reponse into a streamreader
             StreamReader rd = new StreamReader(response.GetResponseStream());
             string data = rd.ReadToEnd();
-            //nothing works from here on down
 
-            //3.JSON object, Json token list
-            JArray nasaJson = JArray.Parse(data);// must parse else would throw the JSON reader exception, its not in JObject format at all
-            ViewBag.Example = nasaJson;//this one doesnt have children, results are formatted differently
-                                       //you know what key to choose if your website has good JSON documentation (its the manual that tells you what youre looking for, where it's nested and how to find it
-
+            JObject nasaJson = JObject.Parse(data);
+            ViewBag.Example = nasaJson;
             rd.Close();
 
-            //4. foreach to cycle through list
-            //List<GiphyModel> output = new List<GiphyModel>(); //dont forget your output
-
-
-
-            //5. return the view with an object
             return View();
         }
     }
